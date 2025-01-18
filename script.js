f341bcf8a5c1b0867b0f2786668ae0f811af896f
ChristianMoreira98 <159092067+ChristianMoreira98@users.noreply.github.com>
2025-01-18 00:00:37 -0300

    update

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+ScrollReveal().reveal('.img-1', {
+    duration:3000, // Duração do efeito (em milisegundos), define a velocidade da transição
+    origin: 'top', // Origem do efeito: o conteúdo virá de baixo pra cima
+    distance: '50px', // Diatância que o elemento "viaja" antes de aparecer
+    });
 
+
+    ScrollReveal().reveal('h1', {
+        duration:3000, // Duração do efeito (em milisegundos), define a velocidade da transição
+        origin: 'right', // Origem do efeito: o conteúdo virá de baixo pra cima
+        distance: '50px', // Diatância que o elemento "viaja" antes de aparecer
+        });
+    
