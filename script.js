c6810ed2b309ced849264589a279d50e48ef5dca
ChristianMoreira98 <159092067+ChristianMoreira98@users.noreply.github.com>
2025-02-03 21:13:39 -0300

    up

--- script.js
+++ script.js
@@ -4,12 +4,4 @@ ScrollReveal().reveal('.img-1', {
     distance: '50px', // Diatância que o elemento "viaja" antes de aparecer
     });
 
-
-        
-    ScrollReveal().reveal('.passado', {
-        duration:3000, // Duração do efeito (em milisegundos), define a velocidade da transição
-        origin: 'left', // Origem do efeito: o conteúdo virá de baixo pra cima
-        distance: '50px', // Diatância que o elemento "viaja" antes de aparecer
-        });
-
        
